da616a30f896ff59aeac03609ad4972bfa9e4b8c
Elodie Chiarani <elodie.chiarani@gmail.com>
2018-11-14 18:18:33 +0100

    Footer done

--- community-app/src/components/Footer/index.js
+++ community-app/src/components/Footer/index.js
@@ -16,7 +16,7 @@ import './footer.scss';
  */
 const Footer = () => (
   <footer id="footer">
-    <img id="footer-img" className="img-fluid w-10" src="src/images/logo_oclock.svg" alt="" />
+    <img id="footer-img" className="img-fluid w-10" src="src/images/logo_oclock_community.svg" alt="" />
     <p id="footer-copyright">2018 &copy; <a href="https://www.linkedin.com/in/elodiechiarani/">Elodie Chiarani</a> / <a href="https://www.linkedin.com/in/thibault-garnier/">Thibault Garnier</a> / <a href="https://www.linkedin.com/in/tristantouchain/">Tristan Touchain</a> / <a href="https://www.linkedin.com/in/vincentsureau/">Vincent Sureau</a></p>
   </footer>
 
