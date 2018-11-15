df00ba406b426eb20fe4e9f0e545d64905a6aa01
Elodie Chiarani <elodie.chiarani@gmail.com>
2018-11-15 13:12:25 +0100

    Change classes in footer and in homepage presentation

--- community-app/src/components/Footer/index.js
+++ community-app/src/components/Footer/index.js
@@ -2,6 +2,7 @@
  * NPM import
  */
 import React from 'react';
+import ClassNames from 'classnames';
 
 /**
  * Local import
@@ -14,12 +15,21 @@ import './footer.scss';
 /**
  * Code
  */
+
+const classcolor = ClassNames(
+  { 'bg-home-navbar': window.location.pathname === '/' },
+  { 'bg-members-navbar': window.location.pathname === '/members' },
+  { 'bg-projects-navbar': window.location.pathname === '/projects' },
+  { 'bg-login-navbar': window.location.pathname === '/login' },
+  { 'bg-member-navbar': window.location.pathname.includes('/members/') },
+  { 'bg-project-navbar': window.location.pathname.includes('/projects/') },
+);
+
 const Footer = () => (
-  <footer id="footer">
+  <footer id="footer" className={classcolor}>
     <img id="footer-img" className="img-fluid w-10" src="src/images/logo_oclock_community.svg" alt="" />
     <p id="footer-copyright">2018 &copy; <a href="https://www.linkedin.com/in/elodiechiarani/">Elodie Chiarani</a> / <a href="https://www.linkedin.com/in/thibault-garnier/">Thibault Garnier</a> / <a href="https://www.linkedin.com/in/tristantouchain/">Tristan Touchain</a> / <a href="https://www.linkedin.com/in/vincentsureau/">Vincent Sureau</a></p>
   </footer>
-
 );
 
 /**
