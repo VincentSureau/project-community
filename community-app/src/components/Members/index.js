2bb64afd91b9e0a5da77af7c665f3e310d6c122d
TristanTouchain <tristan.touchain@me.com>
2018-11-15 17:05:20 +0100

    Bur resolved in membersEdit

--- community-app/src/components/Members/index.js
+++ community-app/src/components/Members/index.js
@@ -16,7 +16,10 @@ import './members.scss';
  */
 const Members = () => (
   <div id="members">
-    Members
+    <section id="members-presentation" className="d-flex flex-column justify-content-center align-items-center bg-h-100vh bg-members-darker">
+      <h1 id="members-presentation-title">Étudiants</h1>
+      <h3 id="members-presentation-subtitle">Vous êtes prêts ? Eux oui !</h3>
+    </section>
   </div>
 
 );
