3f296cf211d457f1c95354356c49ae601c94421a
Elodie Chiarani <elodie.chiarani@gmail.com>
2018-11-15 15:36:44 +0100

    LastMembers + RandomProjects div added

--- community-app/src/components/Home/LastMembers.js
+++ community-app/src/components/Home/LastMembers.js
@@ -15,11 +15,11 @@ import './home.scss';
  * Code
  */
 const LastMembers = () => (
-  <section id="home-members" className="d-flex flex-column justify-content-center align-items-center h-100vh bg-home-darker">
+  <section id="home-members" className="d-flex flex-column justify-content-center align-items-center full-height bg-home-darker">
     <h2 id="home-members-title" className="text-uppercase">Nos derniers poulains</h2>
     <h3 id="home-members-subtitle" className="text-uppercase">Prêts à en découdre !</h3>
     <div id="home-members-list">
-      
+
     </div>
     <button type="button" className="btn btn-lg bg-custom-button text-uppercase">Voir tous les étudiants</button>
   </section>
