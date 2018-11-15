3883cf3dc0d89dea79053d18ee52c4c573251df7
Elodie Chiarani <elodie.chiarani@gmail.com>
2018-11-15 13:43:07 +0100

    Div last members in homepage in progress

---
+++ community-app/src/components/Home/LastMembers.js
@@ -0,0 +1,31 @@
+/**
+ * NPM import
+ */
+import React from 'react';
+
+/**
+ * Local import
+ */
+// Components
+
+// Styles
+import './home.scss';
+
+/**
+ * Code
+ */
+const LastMembers = () => (
+  <section id="home-members" className="d-flex flex-column justify-content-center align-items-center h-100vh bg-home-darker">
+    <h2 id="home-members-title" className="text-uppercase">Nos derniers poulains</h2>
+    <h3 id="home-members-subtitle" className="text-uppercase">Prêts à en découdre !</h3>
+    <div id="home-members-list">
+      
+    </div>
+    <button type="button" className="btn btn-lg bg-custom-button text-uppercase">Voir tous les étudiants</button>
+  </section>
+);
+
+/**
+ * Export
+ */
+export default LastMembers;
--- community-app/src/components/Home/index.js
+++ community-app/src/components/Home/index.js
@@ -7,6 +7,7 @@ import React from 'react';
  * Local import
  */
 // Components
+import LastMembers from './LastMembers';
 
 // Styles
 import './home.scss';
@@ -16,11 +17,12 @@ import './home.scss';
  */
 const Home = () => (
   <div id="home">
-    <div id="home-presentation" className="d-flex flex-column justify-content-center align-items-center">
+    <section id="home-presentation" className="d-flex flex-column justify-content-center align-items-center bg-h-100vh">
       <img id="home-presentation-logo" className="w-25" src="src/images/logo_oclock_community.svg" alt="" />
       <p id="home-presentation-text">Retrouvez tous les <em className="font-weight-bold">étudiants</em> des différentes promotions <em className="font-weight-bold">O'clock</em>, consultez leur profil et admirez l'aboutissement de leur 5 mois de dur labeur : leurs <em className="font-weight-bold">projets</em> de fin d'étude !</p>
       <button type="button" className="btn btn-lg bg-custom-button text-uppercase">C'est parti !</button>
-    </div>
+    </section>
+    <LastMembers />
   </div>
 
 );
