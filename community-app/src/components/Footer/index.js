8d127d015bb9ba26804c5a5efe3124c090b9ffda
Elodie Chiarani <elodie.chiarani@gmail.com>
2018-11-15 15:18:08 +0100

    Footer changed into class

--- community-app/src/components/Footer/index.js
+++ community-app/src/components/Footer/index.js
@@ -15,22 +15,25 @@ import './footer.scss';
 /**
  * Code
  */
+class Footer extends React.Component {
+  render() {
+    const classcolor = ClassNames(
+      { 'bg-home-navfoot': window.location.pathname === '/' },
+      { 'bg-members-navfoot': window.location.pathname === '/members' },
+      { 'bg-projects-navfoot': window.location.pathname === '/projects' },
+      { 'bg-login-navfoot': window.location.pathname === '/login' },
+      { 'bg-member-navfoot': window.location.pathname.includes('/members/') },
+      { 'bg-project-navfoot': window.location.pathname.includes('/projects/') },
+    );
 
-const classcolor = ClassNames(
-  { 'bg-home-navfoot': window.location.pathname === '/' },
-  { 'bg-members-navfoot': window.location.pathname === '/members' },
-  { 'bg-projects-navfoot': window.location.pathname === '/projects' },
-  { 'bg-login-navfoot': window.location.pathname === '/login' },
-  { 'bg-member-navfoot': window.location.pathname.includes('/members/') },
-  { 'bg-project-navfoot': window.location.pathname.includes('/projects/') },
-);
-
-const Footer = () => (
-  <footer id="footer" className={classcolor}>
-    <img id="footer-img" className="img-fluid w-10" src="src/images/logo_oclock_community.svg" alt="" />
-    <p id="footer-copyright">2018 &copy; <a href="https://www.linkedin.com/in/elodiechiarani/">Elodie Chiarani</a> / <a href="https://www.linkedin.com/in/thibault-garnier/">Thibault Garnier</a> / <a href="https://www.linkedin.com/in/tristantouchain/">Tristan Touchain</a> / <a href="https://www.linkedin.com/in/vincentsureau/">Vincent Sureau</a></p>
-  </footer>
-);
+    return (
+      <footer id="footer" className={classcolor}>
+        <img id="footer-img" className="img-fluid w-10" src="src/images/logo_oclock_community.svg" alt="" />
+        <p id="footer-copyright">2018 &copy; <a href="https://www.linkedin.com/in/elodiechiarani/">Elodie Chiarani</a> / <a href="https://www.linkedin.com/in/thibault-garnier/">Thibault Garnier</a> / <a href="https://www.linkedin.com/in/tristantouchain/">Tristan Touchain</a> / <a href="https://www.linkedin.com/in/vincentsureau/">Vincent Sureau</a></p>
+      </footer>
+    );
+  }
+}
 
 /**
  * Export
