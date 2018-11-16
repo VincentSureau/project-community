bec43be2c8e1fbf2f24f729b3d14d88e2fdca6b6
Vincent SUREAU <vincentsureau5@gmail.com>
2018-11-16 14:58:17 +0100

    added CRUD for image

---
+++ Back/src/Controller/Admin/ImageController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Image;
+use App\Form\ImageType;
+use App\Repository\ImageRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/image")
+ */
+class ImageController extends AbstractController
+{
+    /**
+     * @Route("/", name="image_index", methods="GET")
+     */
+    public function index(ImageRepository $imageRepository): Response
+    {
+        return $this->render('admin/image/index.html.twig', ['images' => $imageRepository->findAll()]);
+    }
+
+    /**
+     * @Route("/new", name="image_new", methods="GET|POST")
+     */
+    public function new(Request $request): Response
+    {
+        $image = new Image();
+        $form = $this->createForm(ImageType::class, $image);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($image);
+            $em->flush();
+
+            return $this->redirectToRoute('image_index');
+        }
+
+        return $this->render('admin/image/new.html.twig', [
+            'image' => $image,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="image_show", methods="GET")
+     */
+    public function show(Image $image): Response
+    {
+        return $this->render('admin/image/show.html.twig', ['image' => $image]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="image_edit", methods="GET|POST")
+     */
+    public function edit(Request $request, Image $image): Response
+    {
+        $form = $this->createForm(ImageType::class, $image);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('admin/image_index', ['id' => $image->getId()]);
+        }
+
+        return $this->render('admin/image/edit.html.twig', [
+            'image' => $image,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="image_delete", methods="DELETE")
+     */
+    public function delete(Request $request, Image $image): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$image->getId(), $request->request->get('_token'))) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($image);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('image_index');
+    }
+}
