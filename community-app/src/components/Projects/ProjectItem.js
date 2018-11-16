a858be36dba72ebb30cbc38b5ba6d127a3006929
TristanTouchain <tristan.touchain@me.com>
2018-11-16 11:49:36 +0100

    Link add with NavLink Component

--- community-app/src/components/Projects/ProjectItem.js
+++ community-app/src/components/Projects/ProjectItem.js
@@ -2,7 +2,7 @@
  * NPM import
  */
 import React from 'react';
-
+import { NavLink } from 'react-router-dom';
 /**
  * Local import
  */
@@ -16,10 +16,12 @@ import './projects.scss';
  */
 const ProjectItem = () => (
   <div id="projectitem" className="col-6">
-    <img id="projectitem-photo" src="/src/images/project1.png" alt="" />
-    <h1 id="projectitem-title" className="text-uppercase">Productize</h1>
-    <p id="projectitem-prom" className="text-uppercase">#Krypton</p>
-    <span className="projectitem-tag">Wordpress</span><span className="projectitem-tag">Symfony</span><span className="projectitem-tag">React</span>
+    <NavLink activeClassName="" className="no-blue-on-link" exact to="/projects/productize-1234567890">
+      <img id="projectitem-photo" src="/src/images/project1.png" alt="" />
+      <h1 id="projectitem-title" className="text-uppercase">Productize</h1>
+      <p id="projectitem-prom" className="text-uppercase">#Krypton</p>
+      <span className="projectitem-tag">Wordpress</span><span className="projectitem-tag">Symfony</span><span className="projectitem-tag">React</span>
+    </NavLink>
   </div>
 );
 
