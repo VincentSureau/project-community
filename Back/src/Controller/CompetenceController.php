806553b184b78c15fc1792143328d62ba6684dc8
Vincent SUREAU <vincentsureau5@gmail.com>
2018-11-16 14:15:45 +0100

    crud for competences

---
+++ Back/src/Controller/CompetenceController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Competence;
+use App\Form\CompetenceType;
+use App\Repository\CompetenceRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/competence")
+ */
+class CompetenceController extends AbstractController
+{
+    /**
+     * @Route("/", name="competence_index", methods="GET")
+     */
+    public function index(CompetenceRepository $competenceRepository): Response
+    {
+        return $this->render('competence/index.html.twig', ['competences' => $competenceRepository->findAll()]);
+    }
+
+    /**
+     * @Route("/new", name="competence_new", methods="GET|POST")
+     */
+    public function new(Request $request): Response
+    {
+        $competence = new Competence();
+        $form = $this->createForm(CompetenceType::class, $competence);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($competence);
+            $em->flush();
+
+            return $this->redirectToRoute('competence_index');
+        }
+
+        return $this->render('competence/new.html.twig', [
+            'competence' => $competence,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="competence_show", methods="GET")
+     */
+    public function show(Competence $competence): Response
+    {
+        return $this->render('competence/show.html.twig', ['competence' => $competence]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="competence_edit", methods="GET|POST")
+     */
+    public function edit(Request $request, Competence $competence): Response
+    {
+        $form = $this->createForm(CompetenceType::class, $competence);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('competence_index', ['id' => $competence->getId()]);
+        }
+
+        return $this->render('competence/edit.html.twig', [
+            'competence' => $competence,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="competence_delete", methods="DELETE")
+     */
+    public function delete(Request $request, Competence $competence): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$competence->getId(), $request->request->get('_token'))) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($competence);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('competence_index');
+    }
+}
---
+++ Back/src/Form/CompetenceType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Competence;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CompetenceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('slug')
+            ->add('appUsers')
+            ->add('projects')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Competence::class,
+        ]);
+    }
+}
