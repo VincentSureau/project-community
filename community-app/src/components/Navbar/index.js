0e366cf24d729163ae1fda468e0fce3e035060e9
TristanTouchain <tristan.touchain@me.com>
2018-11-19 13:16:49 +0100

    Project Edit page V1.0

--- community-app/src/components/Navbar/index.js
+++ community-app/src/components/Navbar/index.js
@@ -16,43 +16,40 @@ import './navbar.scss';
 /**
  * Code
  */
-class Navbar extends React.Component {
+const Navbar = () => {
+  const classcolor = ClassNames(
+    { 'home-navfoot': window.location.pathname === '/' },
+    { 'members-navfoot': window.location.pathname === '/members' },
+    { 'projects-navfoot': window.location.pathname === '/projects' },
+    { 'login-navfoot': window.location.pathname === '/login' },
+    { 'member-navfoot': window.location.pathname.includes('/members/') },
+    { 'project-navfoot': window.location.pathname.includes('/projects/') },
+  );
+  const classNavBar = ''.concat('navbar fixed-top navbar-expand navbar-dark bg-', classcolor, ' row');
+  return (
+    <div id="navbar">
+      <nav className={classNavBar}>
+        <div className="col-4">
+          <ul className="navbar-nav">
+            <NavLink activeClassName="" className="nav-item nav-link text-white" exact to="/">Accueil</NavLink>
+            <a className="nav-item nav-link text-white" href="http://oclock.io">O'Clock</a>
+          </ul>
+        </div>
+        <div className="col-4 d-flex justify-content-center">
+          <NavLink activeClassName="" className="w-25 h-25" exact to="/"><img src="/src/images/logo_oclock_community_navbar.svg" alt="Logo Community" /></NavLink>
+        </div>
+        <div className="col-4 d-flex justify-content-end">
+          <ul className="navbar-nav">
+            <NavLink activeClassName="" className="nav-item nav-link text-white" exact to="/projects">Projets</NavLink>
+            <NavLink activeClassName="" className="nav-item nav-link text-white" exact to="/members">Etudiants</NavLink>
+            <NavLink activeClassName="" className="btn btn-outline-white mx-3 btn-border-radius" exact to="/login">Me connecter</NavLink>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
 
-  render() {
-    const classcolor = ClassNames(
-      { 'home-navfoot': window.location.pathname === '/' },
-      { 'members-navfoot': window.location.pathname === '/members' },
-      { 'projects-navfoot': window.location.pathname === '/projects' },
-      { 'login-navfoot': window.location.pathname === '/login' },
-      { 'member-navfoot': window.location.pathname.includes('/members/') },
-      { 'project-navfoot': window.location.pathname.includes('/projects/') },
-    );
-    const classNavBar = ''.concat('navbar fixed-top navbar-expand navbar-dark bg-', classcolor, ' row');
-    return (
-      <div id="navbar">
-        <nav className={classNavBar}>
-          <div className="col-4">
-            <ul className="navbar-nav">
-              <NavLink activeClassName="" className="nav-item nav-link text-white" exact to="/">Accueil</NavLink>
-              <a className="nav-item nav-link text-white" href="http://oclock.io">O'Clock</a>
-            </ul>
-          </div>
-          <div className="col-4 d-flex justify-content-center">
-            <NavLink activeClassName="" className="w-25 h-25" exact to="/"><img src="/src/images/logo_oclock_community_navbar.svg" alt="Logo Community" /></NavLink>
-          </div>
-          <div className="col-4 d-flex justify-content-end">
-            <ul className="navbar-nav">
-              <NavLink activeClassName="" className="nav-item nav-link text-white" exact to="/projects">Projets</NavLink>
-              <NavLink activeClassName="" className="nav-item nav-link text-white" exact to="/members/marc-dubois-1234567890/edit">Test Edit Member</NavLink>
-              <NavLink activeClassName="" className="nav-item nav-link text-white" exact to="/members">Etudiants</NavLink>
-              <NavLink activeClassName="" className="btn btn-outline-white mx-3 btn-border-radius" exact to="/login">Me connecter</NavLink>
-            </ul>
-          </div>
-        </nav>
-      </div>
-    );
-  }
-}
 
 /**
  * Export
--- community-app/src/components/ProjectEdit/index.js
+++ community-app/src/components/ProjectEdit/index.js
@@ -16,7 +16,72 @@ import './projectEdit.scss';
  */
 const ProjectEdit = () => (
   <div id="projectEdit">
-    ProjectEdit
+    <form>
+      <section id="projectedit-form" className="d-flex flex-column justify-content-center align-items-center bg-project">
+        <h1>Productize</h1>
+        <div id="project-project-pc">
+          <div id="project-project-pc-screen">
+            <img src="/src/images/project1.png" alt="" />
+          </div>
+        </div>
+        <input className="mx-2" type="file" name="profile_pic" accept=".jpg, .jpeg, .png" />
+        <div id="projectedit-form-info" className="row justify-content-center">
+          <p className="label col-5">Lien site: </p>
+          <input className="col-5 input-text" type="text" name="web" placeholder="http://productize.com" />
+          <p className="label col-5">Lien vidéo YouTube: </p>
+          <input className="col-5 input-text" type="text" name="youtube" placeholder="http://www.youtube.com" />
+          <textarea className="col-12 input-textarea" type="textarea" name="bio" row="" placeholder="Décrivez ici votre projet" />
+          <p className="label col-5">Technologies utilisées: </p>
+          <div className="col-5 multiselection">
+            <div className="form-check">
+              <div className="form-check-label">
+                <input
+                  type="checkbox"
+                  className="form-check-input"
+                />
+                HTML
+              </div>
+              <div className="form-check-label">
+                <input
+                  type="checkbox"
+                  className="form-check-input"
+                />
+                HTML
+              </div>
+              <div className="form-check-label">
+                <input
+                  type="checkbox"
+                  className="form-check-input"
+                />
+                HTML
+              </div>
+              <div className="form-check-label">
+                <input
+                  type="checkbox"
+                  className="form-check-input"
+                />
+                HTML
+              </div>
+              <div className="form-check-label">
+                <input
+                  type="checkbox"
+                  className="form-check-input"
+                />
+                HTML
+              </div>
+              <div className="form-check-label">
+                <input
+                  type="checkbox"
+                  className="form-check-input"
+                />
+                HTML
+              </div>
+            </div>
+          </div>
+          <button className="col-6 button-submit" type="button">Enregistrer</button>
+        </div>
+      </section>
+    </form>
   </div>
 
 );
