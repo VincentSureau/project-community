335551230561886170f5f208e214f746733ef40c
thibault-gnr <tgarni01@gmail.com>
2018-11-20 13:49:29 +0100

    change competenceadmin controller name

--- Back/src/Admin/CompetenceAdmin.php
+++ Back/src/Admin/CompetenceAdmin.php
@@ -8,7 +8,7 @@ use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
-class CategoryAdmin extends AbstractAdmin
+class CompetenceAdmin extends AbstractAdmin
 {
     protected function configureFormFields(FormMapper $formMapper)
     {
