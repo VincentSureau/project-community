e69bc9f16592af84e21f97595b612770ef8070fc
thibault-gnr <tgarni01@gmail.com>
2018-11-21 11:41:28 +0100

    add filter appuser

--- Back/src/Entity/AppUser.php
+++ Back/src/Entity/AppUser.php
@@ -44,7 +44,7 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
  *         "post"
  *     },
  * )
- * @ApiFilter(SearchFilter::class, properties={"slug": "iexact"})
+ * @ApiFilter(SearchFilter::class, properties={"id": "iexact"})
  * @ORM\Entity(repositoryClass="App\Repository\AppUserRepository")
  * @UniqueEntity("email")
  */
