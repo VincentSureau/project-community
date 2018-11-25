f204917719e708a40afcc04b19569655db650c8a
Vincent SUREAU <vincentsureau5@gmail.com>
2018-11-25 17:31:48 +0100

    fixed: conflict between firewalls

--- Back/src/DataFixtures/AppFixtures.php
+++ Back/src/DataFixtures/AppFixtures.php
@@ -16,16 +16,9 @@ use App\Entity\ProfessionalStatus;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
-    private $passwordEncoder;
-
-    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
-        {
-            $this->passwordEncoder = $passwordEncoder;
-        }
 
     public function load(ObjectManager $manager)
     {
