cc0868ad532216ba10f8dd695b9b5e69052fc2bb
Vincent SUREAU <vincentsureau5@gmail.com>
2018-11-27 15:27:44 +0100

    removed unused subressource

--- Back/src/Entity/AppUser.php
+++ Back/src/Entity/AppUser.php
@@ -3,7 +3,6 @@
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
-use ApiPlatform\Core\Annotation\ApiSubresource;
 use ApiPlatform\Core\Annotation\ApiFilter;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 use Doctrine\Common\Collections\ArrayCollection;
@@ -260,7 +259,6 @@ class AppUser implements UserInterface
     /**
      * @Groups({"user", "userWrite"})
      * @ORM\ManyToMany(targetEntity="App\Entity\Competence", inversedBy="appUsers")
-     * @ApiSubresource
      */
     private $competences;
 
