4f837d95c7ec1317f5e2995dfe3fec7217fb74c3
Vincent SUREAU <vincentsureau5@gmail.com>
2018-11-30 18:07:40 +0100

    test fix isHero field in project image

--- Back/src/Form/ProjectPictureType.php
+++ Back/src/Form/ProjectPictureType.php
@@ -6,6 +6,7 @@ namespace App\Form;
 use App\Entity\Image;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
