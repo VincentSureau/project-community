3af4f7fb65cb471b9675d035e53bc5b183fa5485
TristanTouchain <tristan.touchain@me.com>
2018-11-30 11:14:51 +0100

    ProfilePic now with upload file

--- community-app/src/components/MemberEdit/index.js
+++ community-app/src/components/MemberEdit/index.js
@@ -51,7 +51,7 @@ class MemberEdit extends React.Component {
   onChangeFile(evt) {
     const { member } = this.props;
     const fd = new FormData();
-    if (evt.target.files[0].size < 1000000) {
+    if (evt.target.files[0].size < 500000) {
       fd.append('file', evt.target.files[0], evt.target.files[0].name);
       axios.post(`http://127.0.0.1:8001/app_users/${member.id}/profil_picture`, fd, {
         headers: {
