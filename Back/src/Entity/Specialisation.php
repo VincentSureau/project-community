1c9525811732fc1c04939edf5c286cbdba33a08b
Vincent SUREAU <vincentsureau5@gmail.com>
2018-12-03 12:05:14 +0100

    added api ressources

--- Back/src/Entity/Specialisation.php
+++ Back/src/Entity/Specialisation.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiProperty;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
