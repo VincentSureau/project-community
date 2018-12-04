e110f6efd7d954ce84261bebf2ae178bb3d04824
thibault-gnr <tgarni01@gmail.com>
2018-12-04 09:19:33 +0100

    swagger_ui false added + fixed description fixture

--- Back/src/DataFixtures/AppFixtures.php
+++ Back/src/DataFixtures/AppFixtures.php
@@ -277,7 +277,7 @@ class AppFixtures extends Fixture
         $promoKrypton->setEndDate(new \DateTime());
         $manager->persist($promoKrypton);
         $community = new Project;
-        $community->setDescription('L\'objectif du projet était de réaliser un site internet communautaire pour les anciens étudiants d\' O\’clock : un mélange de LinkedIn et des Copains d’Avant (une partie étant publique et l’autre privée).
+        $community->setDescription('L\'objectif du projet était de réaliser un site internet communautaire pour les anciens étudiants d\' O\'clock : un mélange de LinkedIn et des Copains d’Avant (une partie étant publique et l’autre privée).
         Il s’agit d’une plateforme permettant de référencer tous les étudiants et anciens étudiants d\’ O\'clock en recensant leurs différents profils et projets.
         Plusieurs avantages :
             Pour les personnes qui se projettent de s’inscrire : être convaincu en consultant les profils, projets et débouchés professionnels des anciens élèves d’O’Clock.
